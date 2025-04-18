d7e7d57954195a03d051b4b4208b567acfbfd0f2
Marcin <marcin.golec1995@gmail.com>
2025-04-18 15:11:58 +0200

    titlebar modified

--- apps/cost-tracker/src/components/Titlebar.tsx
+++ apps/cost-tracker/src/components/Titlebar.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 
 const Titlebar = () => (
-  <div className="sticky top-0 bg-linear-to-r from-emerald-400 via-emerald-200 to-gray-300 py-8 shadow-md">
-    <div className="container flex items-baseline mx-auto">
+  <div className="sticky top-0 bg-linear-to-r from-emerald-100 via-emerald-200 to-emerald-50 py-8 shadow-md">
+    <div className="container flex items-center mx-auto">
       <h1 className="text-3xl font-bold font-stretch-expanded xl:w-1/4">
         Cost tracker
       </h1>
       <div className="ml-10 flex gap-x-10 text-lg font-semibold">
-        <Link href="/users" className="hover:text-gray-500">
+        <Link href="/users" className="hover:underline">
           Users
         </Link>
-        <Link href="/costs" className="hover:text-gray-500">
+        <Link href="/costs" className="hover:underline">
           Costs
         </Link>
-        <Link href="/categories" className="hover:text-gray-500">
+        <Link href="/categories" className="hover:underline">
           Categories
         </Link>
       </div>
