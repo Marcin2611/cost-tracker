a0f0e178261371992f5ab33dccbbdd1168a8450c
Marcin <marcin.golec1995@gmail.com>
2025-04-28 15:18:33 +0200

    dynamic cols object created

--- apps/cost-tracker/src/components/Table.tsx
+++ apps/cost-tracker/src/components/Table.tsx
@@ -1,5 +1,20 @@
 import React, { ReactNode } from 'react';
 
+const colsTailwindClass: { [k: number]: string } = {
+  1: 'grid-cols-1',
+  2: 'grid-cols-2',
+  3: 'grid-cols-3',
+  4: 'grid-cols-4',
+  5: 'grid-cols-5',
+  6: 'grid-cols-6',
+  7: 'grid-cols-7',
+  8: 'grid-cols-8',
+  9: 'grid-cols-9',
+  10: 'grid-cols-10',
+  11: 'grid-cols-11',
+  12: 'grid-cols-12',
+};
+
 export const Table = ({ children }: { children: ReactNode[] }) => {
   return (
     <div className="flex flex-col w-full max-h-[calc(100%-44px)] overflow-y-scroll">
@@ -10,7 +25,7 @@ export const Table = ({ children }: { children: ReactNode[] }) => {
 
 const Header = ({ children }: { children: ReactNode[] }) => {
   return (
-    <div className={`grid grid-cols-${children.length} border-b-2`}>
+    <div className={`grid ${colsTailwindClass[children.length]} border-b-2`}>
       {children}
     </div>
   );
@@ -31,7 +46,11 @@ const Body = ({ children }: { children: ReactNode[] }) => {
 };
 
 const Row = ({ children }: { children: ReactNode[] }) => {
-  return <div className={`grid grid-cols-${children.length}`}>{children}</div>;
+  return (
+    <div className={`grid ${colsTailwindClass[children.length]}`}>
+      {children}
+    </div>
+  );
 };
 
 const Cell = ({ children }: { children: ReactNode[] | ReactNode }) => {
